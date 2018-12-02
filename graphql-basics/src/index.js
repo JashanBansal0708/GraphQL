2cc2cbdd7e0b13de2deeda82cdcad0922e22cfa5
jashanbansal0708 <jashanbansal7856@yahoo.com>
2018-12-02 17:30:45 +0530

    Install graphql-yoga. Set up the GraphQL server for the first time and query very basic data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from "graphql-yoga"
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        name : String!
+        location : String!
+        bio : String!
+    }
+
+`
+// Resolvers
+const resolvers = {
+    Query : {
+        
+        name(){
+            return 'Jashan Bansal'
+        },
+
+        location(){
+            return 'Mansa'
+        },
+
+        bio(){
+            return 'I am passionate for coding'    
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+ server.start(() => {
+     console.log('The server is UP!');
+ })
